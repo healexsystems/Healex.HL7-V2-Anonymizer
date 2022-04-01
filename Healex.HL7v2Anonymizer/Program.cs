8ec6453ad871fe6edc6ff493fa5619110ea81f51
Wilson Edgar <wilsonedgar@gmail.com>
2022-04-01 08:59:31 +0100

    non functional commit

--- Healex.HL7v2Anonymizer/Program.cs
+++ Healex.HL7v2Anonymizer/Program.cs
@@ -3,45 +3,41 @@ using HL7.Dotnetcore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using CommandLine;
+using Healex.HL7v2Anonymizer.Options;
+namespace Healex.HL7v2Anonymizer {
+    class Program {
 
-namespace Healex.HL7v2Anonymizer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            WaitForInput();
+        static void Main(string[] args) {
+            try {
+                var input = args.Select(s => s.Trim());
+                Parser.Default
+                     .ParseArguments<Process>(input)
+                     .MapResult((Process options) =>  ProcessCommandHandler(options),
+                                 error => -1);
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error while processing commad.\nMessage:{ex.Message} \nStack{ex.StackTrace}");
+            }
         }
 
-        private static void WaitForInput()
-        {
-            Console.WriteLine("Welcome to the Healex HL7v2 anonymizer!");
-            Console.WriteLine("---------------------------------------------------------------------------------------");
-            Console.WriteLine("WARNING: THIS APPLICATION OVERWRITES THE ORIGINAL v2 MESSAGES. BACKUP YOUR FILES FIRST!");
-            Console.WriteLine("---------------------------------------------------------------------------------------");
-            Console.WriteLine("Enter the directory to your v2 messages and press enter:");
-            var directory = Console.ReadLine();
-            Console.WriteLine();
-            TryAnonymizeMessages(directory);
-            Console.WriteLine();
-            WaitForInput();
+        private static int ProcessCommandHandler(Process options) {
+            TryAnonymizeMessages(options.Directory);
+            return 1;
         }
 
-        private static void TryAnonymizeMessages(string directory)
-        {
+        private static void TryAnonymizeMessages(string directory) {
             var pathsToV2Messages = GetPathsToV2Messages(directory);
             var anonymizer = new Anonymizer(GetReplacementOptions());
-
-            if (pathsToV2Messages is not null)
-            {
-                if (pathsToV2Messages.Length == 0)
+            if (pathsToV2Messages is not null) {
+                if (pathsToV2Messages.Length == 0) {
                     Console.WriteLine($"=> No v2 messages found in {directory}");
-
-                foreach (string path in pathsToV2Messages)
-                {
+                }
+                foreach (string path in pathsToV2Messages) {
                     var message = ReadAndParseMessage(path);
-                    if (message is not null)
-                    {
+                    if (message is not null) {
                         var success = anonymizer.Anonymize(message);
                         SerializeAndWriteMessageOrLogError(success, message, path);
                     }
@@ -49,50 +45,41 @@ namespace Healex.HL7v2Anonymizer
             }
         }
 
-        private static void SerializeAndWriteMessageOrLogError(bool success, Message message, string path)
-        {
-            if (success)
-            {
+        private static void SerializeAndWriteMessageOrLogError(bool success, Message message, string path) {
+            if (success) {
                 Console.WriteLine($"=> Anonymization successful: {path}");
                 var serializedMessage = message.SerializeMessage(true);
                 File.WriteAllText(path, serializedMessage);
             }
-            else
-            {
+            else {
                 Console.WriteLine($"=> Anonymization fail: {path}");
             }
         }
 
-        private static string[] GetPathsToV2Messages(string directory)
-        {
-            try
-            {
+        private static string[] GetPathsToV2Messages(string directory) {
+            try {
                 var v2Messages = Directory.GetFiles(directory, "*.hl7");
                 return v2Messages;
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine(e.Message);
                 return null;
             }
         }
 
-        private static Message ReadAndParseMessage(string path)
-        {
-            try
-            {
+        private static Message ReadAndParseMessage(string path) {
+            try {
                 var message = new Message(File.ReadAllText(path));
                 message.ParseMessage();
                 return message;
-            } catch (Exception e)
-            {
+            }
+            catch (Exception e) {
                 Console.Write(e.Message);
                 return null;
             }
         }
 
-        private static ReplacementOptions GetReplacementOptions()
-        {
+        private static ReplacementOptions GetReplacementOptions() {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false);
--- Healex.HL7v2Anonymizer/Services/Anonymizer.cs
+++ Healex.HL7v2Anonymizer/Services/Anonymizer.cs
@@ -3,87 +3,88 @@ using System;
 using System.Linq;
 using static Healex.HL7v2Anonymizer.ReplacementOptions;
 
-namespace Healex.HL7v2Anonymizer.Services
-{
-    public class Anonymizer
-    {
-        private readonly ReplacementOptions _replacementOptions;
+namespace Healex.HL7v2Anonymizer.Services {
 
-        public Anonymizer(ReplacementOptions replacementOptions)
-        {
-            _replacementOptions = replacementOptions;
+    public class Anonymizer {
+
+        #region Private Members
+
+        private readonly ReplacementOptions options;
+
+        #endregion
+
+        #region Constructors
+
+        public Anonymizer(ReplacementOptions options) {
+            this.options = options;
         }
 
-        public bool Anonymize(Message message)
-        {
-            var isSuccess = true;
+        #endregion
 
-            for (var segmentIndex = 0; segmentIndex < message.SegmentCount; segmentIndex++)
-            {
-                var segment = message.Segments().ElementAt(segmentIndex);
-                if (_replacementOptions.Segments.FirstOrDefault(segRep => segRep.Segment == segment.Name)
-                    is { } segmentReplacement)
-                {
+        #region Public API
+
+        public bool Anonymize(Message message) {
+            var isSuccess = true;
+            for (var index = 0; index < message.SegmentCount; index++) {
+                var segment = message.Segments()[index]; 
+                var replacements = options.Segments.FirstOrDefault(s => s.Segment == segment.Name);
+                if (replacements != null) {
+                    message.Segments()[index] = new Segment("DummySegment", new HL7Encoding());
+                }
+
+
+                if (options.Segments.FirstOrDefault(segRep => segRep.Segment == segment.Name) is { } segmentReplacement) {
                     // Create new temporary message for each repeating segment 
                     // because we can't set values in all repeating segments at once
-                    var tempMessage = AddSegmentAtIndex(segment, segmentIndex);
-                    foreach (Replacement replacement in segmentReplacement.Replacements)
-                    {
+                    var tempMessage = AddSegmentAtIndex(segment, index);
+                    foreach (Replacement replacement in segmentReplacement.Replacements) {
                         var replacementValue = GetReplacementValue(replacement, message);
                         isSuccess = TryReplaceValue(tempMessage, replacement.Path, replacementValue);
                     }
                 }
             }
-
             return isSuccess;
         }
 
-        private Message AddSegmentAtIndex(Segment segment, int segmentIndex)
-        {
+        private Message AddSegmentAtIndex(Segment segment, int index) {
             var tempMessage = new Message();
-
             // workaround to ensure the segment gets it absolute (internal) SequenceNo re-assigned in AddNewSegment()
-            for (var i = 0; i < segmentIndex; i++)
-            {
+            for (var i = 0; i < index; i++) {
                 tempMessage.AddNewSegment(new Segment("DummySegment", new HL7Encoding()));
             }
             tempMessage.AddNewSegment(segment);
             return tempMessage;
         }
 
-        private string GetReplacementValue(Replacement replacement, Message message)
-        {
-            if (replacement.Value == "HASH")
-            {
-                try
-                {
+        private string GetReplacementValue(Replacement replacement, Message message) {
+            if (replacement.Value == "HASH") {
+                try {
                     var valueToHash = message.GetValue(replacement.Path);
                     var hashedValue = HashGenerator.HashString(valueToHash);
                     return hashedValue;
-                } catch
-                {
+                }
+                catch {
                     // Could not find a value to hash in the HL7 message
                 }
             }
             return replacement.Value;
         }
 
-        private bool TryReplaceValue(Message message, string path, string replacementValue)
-        {
-            try
-            {
+        private bool TryReplaceValue(Message message, string path, string replacementValue) {
+            try {
                 message.SetValue(path, replacementValue);
             }
-            catch (HL7Exception)
-            {
+            catch (HL7Exception) {
                 // Throws if segment is not present
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine(e.Message);
                 return false;
             }
             return true;
         }
+
+        #endregion
+
     }
 }
